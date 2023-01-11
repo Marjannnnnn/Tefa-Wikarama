d7920bb8b00a242ee10a1d15a0e72baf9f8dd009
Marjan02 <kevinzevalja@gmail.com>
2023-01-11 23:10:03 +0700

    asu

--- lib/utils/revalidateCache.ts
+++ lib/utils/revalidateCache.ts
@@ -1,13 +1,25 @@
 export default function revalidateCache(id: string, paths: string[]) {
-    return fetch("/api/revalidate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id, paths }),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            return data;
-        });
+  const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key: any, value: any) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+  };
+  return fetch("/api/revalidate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, paths }, getCircularReplacer()),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
 }
