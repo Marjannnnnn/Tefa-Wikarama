d44877f6fe98830bcde3b4fd9a3a1d57f6048988
Marjan02 <kevinzevalja@gmail.com>
2023-01-11 22:04:21 +0700

    deploy

--- components/seo/AboutSEO.tsx
+++ components/seo/AboutSEO.tsx
@@ -8,20 +8,10 @@ export default function AboutSEO() {
     return (
         <Head>
             <title>Tefa Wikrama</title>
-            <meta name="description" content="" />
-            <meta
-                name="keywords"
-                content="Teaching Factory"
-            />
             <meta name="author" content="Wikrama" />
-            <meta property="og:title" content="Wikrama" />
-            <meta property="og:description" content={description} />
-
-            <meta property="og:image" content="/profile.png" />
-            {/* for twitter */}
-            <meta name="twitter:title" content="Wikrama" />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content="/profile.png" />
+            <meta property="title" content="Wikrama" />
+            <meta property="description" content={description} />
+            <meta property="image" content="/profile.png" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
     );
--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: [
-            "firebasestorage.googleapis.com",
-            "images.unsplash.com",
-            "media4.giphy.com",
-            "www.smkwikrama.sch.id",
-            "play-lh.googleusercontent.com",
-            "1.bp.blogspot.com"
-        ],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "images.unsplash.com",
+      "media4.giphy.com",
+      "www.smkwikrama.sch.id",
+      "play-lh.googleusercontent.com",
+      "1.bp.blogspot.com",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
