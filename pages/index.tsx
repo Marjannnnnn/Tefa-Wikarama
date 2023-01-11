b9e6f759a68d4b581c6e8721b957242cf631e123
Marjan02 <kevinzevalja@gmail.com>
2023-01-11 22:54:32 +0700

    waduh

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,18 +18,18 @@ const Home = ({ posts }: Props) => {
 
   // const latestPost = posts[0];
   const showDummy = posts.slice(1).length % 3 !== 0;
-  //   if (!posts.length) {
-  //     return <Notfound />;
-  //   } else {
-  return (
-    <>
-      <HomeSEO />
-      <div className="bg-primary min-h-screen p-5 xl:p-0 w-full">
-        <div className="max-w-7xl mx-auto pt-10 pb-36">
-          <p className="my-10 text-4xl">
-            Daftar Produk Teaching Factory SMK Wikrama :
-          </p>
-          {/* <Link href={`/${latestPost?.id}`}>
+  if (!posts.length) {
+    return <Notfound />;
+  } else {
+    return (
+      <>
+        <HomeSEO />
+        <div className="bg-primary min-h-screen p-5 xl:p-0 w-full">
+          <div className="max-w-7xl mx-auto pt-10 pb-36">
+            <p className="my-10 text-4xl">
+              Daftar Produk Teaching Factory SMK Wikrama :
+            </p>
+            {/* <Link href={`/${latestPost?.id}`}>
                         <div className="h-99 bg-secondary mb-10 rounded-md p-5 sm:px-20 py-20 flex justify-around items-center gap-20 group hover:ring-4 ring-blue-500 transition-all flex-col sm:flex-row  cursor-pointer">
                             <div className="w-full sm:w-96  h-full flex flex-col justify-between">
                                 <div>
@@ -64,45 +64,45 @@ const Home = ({ posts }: Props) => {
                             </div>
                         </div>
                     </Link> */}
-          <div className="flex gap-10 flex-wrap justify-center md:justify-between rounded-md">
-            {/* {posts.slice(1).map((post, index) => { */}
-            {posts.map((post, index) => {
-              return (
-                <Link href={`/${post.id}`} key={index}>
-                  <div className="w-96 group cursor-pointer">
-                    <div className="w-full h-96 mb-5 rounded-md relative group-hover:ring-4 ring-blue-500 transition-all">
-                      <Image
-                        src={post.imageUrl as string}
-                        alt={post.title}
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        className="rounded-md "
-                        priority
-                        placeholder="blur"
-                        blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
-                      />
+            <div className="flex gap-10 flex-wrap justify-center md:justify-between rounded-md">
+              {/* {posts.slice(1).map((post, index) => { */}
+              {posts.map((post, index) => {
+                return (
+                  <Link href={`/${post.id}`} key={index}>
+                    <div className="w-96 group cursor-pointer">
+                      <div className="w-full h-96 mb-5 rounded-md relative group-hover:ring-4 ring-blue-500 transition-all">
+                        <Image
+                          src={post.imageUrl as string}
+                          alt={post.title}
+                          layout="fill"
+                          objectFit="cover"
+                          objectPosition="center"
+                          className="rounded-md "
+                          priority
+                          placeholder="blur"
+                          blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+                        />
+                      </div>
+                      <p className="text-sm text-gray-400">
+                        {post.createdAt as string}
+                      </p>
+                      <h1 className="text-2xl text-gray-300 mt-2 font-bold">
+                        {post.title}
+                      </h1>
                     </div>
-                    <p className="text-sm text-gray-400">
-                      {post.createdAt as string}
-                    </p>
-                    <h1 className="text-2xl text-gray-300 mt-2 font-bold">
-                      {post.title}
-                    </h1>
-                  </div>
-                </Link>
-              );
-            })}
-            {showDummy && (
-              <div className="hidden w-96 h-72 text-2xl sm:flex justify-center items-center text-center p-5 rounded-md"></div>
-            )}
+                  </Link>
+                );
+              })}
+              {showDummy && (
+                <div className="hidden w-96 h-72 text-2xl sm:flex justify-center items-center text-center p-5 rounded-md"></div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
-// };
 
 export default Home;
 
